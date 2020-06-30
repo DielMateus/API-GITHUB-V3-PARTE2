95dbd421c3e59eb935a3011b6c32ccf51507648f
Diel Mateus Peruzzo <67617863+DielMateus@users.noreply.github.com>
2020-06-30 15:27:08 -0300

    Update teste.js

--- js/teste.js
+++ js/teste.js
@@ -1,38 +1,4 @@
-/*function carConteudo(){
-    document.getElementById("conteudo").innerHTML;
-    console.log("teste");
-}
-*/
 
-// $(document).ready(function () {
-//     $('#foto,#mostrar').click(function () {
-//         var valor = $('#mostrar').attr('value');
-//
-//         var url = 'https://api.github.com/repos/' + valor;
-//         $.ajax({
-//             type: 'GET',
-//             url: url,
-//             success: function (data) {
-//                 var dados = JSON.stringify($('#mostrar').attr('value'));;
-//                 sessionStorage.setItem('chave', dados );
-//                 window.location.replace("buscadeUsuarios.html");
-//                 var statusHTML = '';
-//                 $.each(data, function (i, status) {
-//
-//                     statusHTML += '<div class="col-12 col-sm-4">';
-//                     statusHTML += '<div class="card"  style="width: 18rem;">';
-//                     statusHTML += `<img src="${status.owner.avatar_url}"/>`;
-//                     statusHTML += '<div class="card-body" id="repositorio">';
-//                     statusHTML += `<h5 class="card-title" id="fullname">${status.full_name}</h5>`;
-//                     statusHTML += '</div>';
-//                     statusHTML += '</div>';
-//                     statusHTML += '</div>';
-//                 });
-//                 document.getElementById('result').innerHTML = statusHTML;
-//             }
-//         })
-//     })
-// });
 function mostrarRepo(element) {
     //linha que pega o valor do nome para passar para o link abaixo
     var valor = element.value;
